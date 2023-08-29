587b5f4ddf5562209dfb2eb1382e93200cedbf5c
Kauavitor18 <kaua04082004@gmail.com>
2023-08-29 15:51:39 -0300

    atualizando codigo

--- lista.py
+++ lista.py
@@ -30,17 +30,54 @@ cal = c + 1.8 + 32
 f = cal
 print(f"Fahrenheit {f} e Celsius {c}\n")
 
+#5
+
+
 idade = int(input("Digite sua idade: "))
 if(idade >= 18):
     print("Você tem idadee suficiente")
 else:
     print("Não está apto! \n")
 
-#5
+#6
 
-vel = int(input("Velocidade do carro: ")
+vel = int(input("Velocidade do carro: "))
 if( vel > 80):
-    print("você foi multado")
-else:
-    print("Nã foi multado")
+    multa = 7*(vel - 80)
+    print(f"você foi multado, sua multa é de {multa}\n")
     
+else:
+    print("Nã foi multado \n")
+
+
+#7 tentando resolver
+
+#num1 = int(input("Digite um numero: "))
+#num2 = int(input("Digite um numero: "))
+#num3 = int(input("Digite um numero: "))
+#if(num1 > num2 >num3):
+    #print(f"Numero {num1} é maior que {num2} e o nm
+
+#8
+
+pre = int(input("Quantas canetas pretas você deseja: "))
+azu = int(input("Quantas canetas pretas você deseja: "))
+quantp = pre * 2.50
+quanta = azu * 2
+valor = quantp + quanta
+print(f"O valor total é ${valor}")
+    
+#9
+
+nome1 = str(input("Digite your name: "))
+nome2 = str(input("Digite your name: "))
+nome3 = str(input("Digite your name: "))
+print(f"Olá meu nome é {nome1}, Olá meu nome é {nome2}, Olá meu nome é {nome3}")
+
+
+#finish lista 1  e 2 
+
+
+
+
+
---
+++ lista03.py
@@ -0,0 +1,40 @@
+
+#1
+
+for i in range(0, 11):
+    print(i)
+    
+
+#2
+
+for i in range(10, -1, -1):
+    print(i)
+    
+#3
+
+for i in range(0, 12, 2):
+    print(i)
+    
+#4
+
+num = int(input("Digite um numero: "))
+for i in range(0, 10):
+    print(num+i)
+    
+#5
+
+nome = input("Digite um nome: ")
+
+while nome != "parar":
+    nome = input("Digite um nome: ")
+
+    
+#6 #Completar
+
+num = int(input("Digite um numero: "))
+
+while num != 0:
+    print("Digite um numero: ")
+
+
+#7 #completar
